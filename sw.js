a0a318b86d24d2a41d476fd679de86b63016fe90
hugosuporte <hugosuporte00@gmail.com>
2026-01-06 08:33:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "contador-bichos-v5";
+const CACHE = "contador-bichos-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,24 +7,20 @@ const ASSETS = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k!==CACHE ? caches.delete(k):null))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
